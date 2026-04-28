0c937e36bc2a8f62050a55e7f443ee70f61aab2f
StorywithLove <48684315+StorywithLove@users.noreply.github.com>
2026-04-28 19:16:27 +0800

    Update zjemc_selenium.py

--- zjemc_selenium.py
+++ zjemc_selenium.py
@@ -30,6 +30,7 @@ def get_zjemc(web_url, usr, pwd):
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")    # 设置浏览器窗口大小 
         options.add_argument("--disable-gpu")
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         driver.get(web_url)
